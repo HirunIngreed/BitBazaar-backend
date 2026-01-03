3645d25ace6fbffd98da813ba76fb5451721d820
Hirun <hingreed738@gmail.com>
2026-01-03 11:34:21 +0530

    cors 🖋️

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "https://bit-bazaar-frontend-8qak-b49v9gon6-hirun-ingreeds-projects.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 
 app.use(bodyParser.json())
 
